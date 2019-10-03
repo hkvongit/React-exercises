55dfb81689c6260c802f67a8da3ac72fbe8b6684
hkvongit <brosgiant@gmail.com>
2019-10-03 12:36:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 
 function App() {
+  let state = true
+  let stateString = state.toString()
+  const conditionalRender =() =>{
+    if(state){
+      return(
+        <div>the state is {stateString}</div>
+      )
+    }else{
+      return(
+        <div>{stateString} state</div>
+      )
+    }
+  }
   return (
     <div className="App">
       App
-  
+      {conditionalRender()}
     </div>
   );
 }
